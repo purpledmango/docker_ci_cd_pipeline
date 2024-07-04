8b30c36d54e5d151e0c7b890bf030713db71a82a
purpledmango <nitishmerlin@gmail.com>
2024-07-04 10:09:17 +0530

    intial push

---
+++ server.js
@@ -0,0 +1,13 @@
+const express  = require('express');
+
+const app = express();
+
+const PORT = 3000
+
+app.get('/', (req, res)=>{
+	res.status(200)
+	res.send("Hello");
+})
+
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT} `));
